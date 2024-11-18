1d5e7b27a46a8080ad601f684d8dbb64838a4c38
Terry Leggett II <terryleggettjr@gmail.com>
2024-11-18 08:57:35 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Terry is the champion'
